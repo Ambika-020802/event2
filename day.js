f227277aa0ccd66c17f0aa71c7033b19589d7e75
Ambika-020802 <ambikarekandar2002@gmail.com>
2024-11-20 13:55:54 +0530

    Add files via upload

---
+++ day.js
@@ -0,0 +1,159 @@
+////event bubbling & event trickling
+
+// const grand=document.getElementById("grand")
+// grand.addEventListener('click',(e) =>{
+//     e.stopPropagation();
+//     console.log("grand parents cli//     },3000)
+    
+// },false)
+
+// const parents=document.getElementById("parent")
+// parents.addEventListener('click',(e)=>{
+//     e.stopPropagation();
+//     setTimeout(()=>{
+//         console.log("parents clicked");
+//         parents.style.backgroundColor="yellow"
+//     },2000)
+    
+// },false)
+
+// const child=document.getElementById("child")
+// child.addEventListener('click',(e)=>{
+//     e.stopPropagation();
+//     setTimeout(() => {
+//         console.log("child clicked");
+//         child.style.backgroundColor="green"
+//     }, 1000);
+   
+    
+// },true)cked");
+    
+// })
+
+
+// // const parents=document.getElementById("parent")
+// // parents.addEventListener('click',(e)=>{
+//     e.stopPropagation();
+//     console.log("parents clicked");
+    
+// })
+
+// const child=document.getElementById("child")
+// child.addEventListener('click',(e)=>{
+//     e.stopPropagation();
+//     console.log("child clicked");
+    
+// })
+
+
+// const grand=document.getElementById("grand")
+// grand.addEventListener('click',(e) =>{
+//     e.stopPropagation();
+//     setTimeout(()=>{
+//         console.log("grand parents clicked");
+//         grand.style.backgroundColor="red"
+//     },3000)
+    
+// })
+
+// const parents=document.getElementById("parent")
+// parents.addEventListener('click',(e)=>{
+//     e.stopPropagation();
+//     setTimeout(()=>{
+//         console.log("parents clicked");
+//         parents.style.backgroundColor="yellow"
+//     },2000)
+    
+// })
+
+// const child=document.getElementById("child")
+// child.addEventListener('click',(e)=>{
+//     e.stopPropagation();
+//     setTimeout(() => {
+//         console.log("child clicked");
+//         child.style.backgroundColor="green"
+//     }, 1000);
+   
+    
+// })
+
+
+
+// const grand=document.getElementById("grand")
+// grand.addEventListener('click',(e) =>{
+//     e.stopPropagation();
+//     setTimeout(()=>{
+//         console.log("grand parents clicked");
+//         grand.style.backgroundColor="red"
+
+
+
+
+
+
+
+
+
+
+// const grand=document.getElementById("grand")
+// grand.addEventListener('click',(e) =>{
+//         console.log("grand parents clicked");
+    
+// },true)
+
+// const parents=document.getElementById("parent")
+// parents.addEventListener('click',(e)=>{
+//            console.log("parents clicked");
+    
+// },true)
+
+// const child=document.getElementById("child")
+// child.addEventListener('click',(e)=>{
+   
+//         console.log("child clicked");
+   
+    
+// },false)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
